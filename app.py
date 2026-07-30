6661385bbd9e1870608bc252085ba4547465c590
Sudhansu Ranjan <sranjan2708@gmail.com>
2026-07-30 23:43:35 +0530

    implemented HTML form and file upload section

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET" , "POST"])
 def home():
 
-    students = [
-        "Rahul",
-        "Priya",
-        "Aman",
-        "Neha"
-    ]
-    return render_template("index.html", students = students)
+    if request.method == "POST":
+
+        uploaded_file = request.files["marks"]
+
+        return render_template(
+            "index.html",
+            message = f"{uploaded_file.filename} uploaded successfully!"
+        )
+
+    return render_template("index.html", message=None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
